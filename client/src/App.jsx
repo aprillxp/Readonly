504f096f0c602e4a2836f8fbf5790f31968d903c
unknown <gavrilariendra00@gmail.com>
2024-09-13 23:28:05 +0700

    chore: client setup and component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { Routes, Route, Router } from "react-router-dom";
+import Home from "./pages/Home";
+import CreateBooks from "./pages/CreateBooks";
+import ShowBooks from "./pages/ShowBooks";
+import UpdateBooks from "./pages/UpdateBooks";
+import DeleteBooks from "./pages/DeleteBooks";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path="/" element={Home} />
+      <Route path="/books/create" element={CreateBooks} />
+      <Route path="/books/details/:id" element={ShowBooks} />
+      <Route path="/books/update/:id" element={UpdateBooks} />
+      <Route path="/books/delete/:id" element={DeleteBooks} />
+    </Routes>
+  );
 };
 
 export default App;
+
+// 27:00
